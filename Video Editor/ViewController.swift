316bc8e5e6b6549f7c07ae793fa1612619b577a7
Tatsuya Suganuma <suganuma@rokunana.com>
2014-07-09 14:09:53 +0900

    Initial Commit

---
+++ Video Editor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Video Editor
+//
+//  Created by Tatsuya Suganuma on 2014/07/09.
+//  Copyright (c) 2014年 redmorado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
